b87083954f10b450fbf562a48b506e54acffd197
Alexck07 <45398897+Alexck07@users.noreply.github.com>
2020-07-24 23:08:02 -0500

    Add files via upload

---
+++ Tiendita/models/Detalle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tiendita.models
+{
+    class Detalle
+    {
+        public uint Id { get; set; }
+        public uint ProductoId { get; set; }
+        public Producto producto { get; set; }
+        public uint VentaId { get; set; }
+        public Venta Venta { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+}
---
+++ Tiendita/models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tiendita.models
+{
+    class Usuario
+    {
+        public uint Id { get; set; }
+        public string Nombre { get; set; }
+        public string Contrasenia { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}) Nombre: {Nombre} Contraseña: {Contrasenia}";
+        }
+    }
+}
